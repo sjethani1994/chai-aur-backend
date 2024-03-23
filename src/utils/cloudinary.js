0b46c0e719d819f92180d625054c8df3a87ea14f
Sumit Jethani <sjethani651@gmail.com>
2024-03-23 19:31:45 +0530

    commit with register controller code added and images uploaded successfully

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -14,8 +14,6 @@ const uploadOnCloudinary = async (localFilePath) => {
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    // file has been uploaded successfull
-    //console.log("file is uploaded on cloudinary ", response.url);
     fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
