fe793183e47da0ebd7b025446ccbd635f95fb9fe
Sumit Jethani <sjethani651@gmail.com>
2024-03-23 13:43:03 +0530

    user register controller added and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import jwt from "json-web-token";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 const userSchema = new Schema(
   {
     username: {
@@ -52,7 +52,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
