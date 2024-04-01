f38feb3fb55501f8bd40109ccb3e8bbe8c141110
Sumit Jethani <sjethani651@gmail.com>
2024-04-01 14:56:15 +0530

    comment and like controller implemented

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import { isValidObjectId } from "mongoose";
 import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -11,7 +11,33 @@ const getVideoComments = asyncHandler(async (req, res) => {
 });
 
 const addComment = asyncHandler(async (req, res) => {
-  // TODO: add a comment to a video
+  try {
+    // TODO: add a comment to a video
+    const { videoId } = req.params;
+
+    if (!isValidObjectId(videoId)) {
+      throw new ApiError(400, "Invalid Video ID");
+    }
+
+    const { content } = req.body;
+    if (!content || typeof content !== "string") {
+      throw new ApiError(
+        400,
+        "Content field must be provided and of type string."
+      );
+    }
+
+    const comment = await Comment.create({
+      content,
+      video: videoId,
+      owner: req.user?._id,
+    });
+    return res
+      .status(200)
+      .json(new ApiResponse(200, comment, "Comment added successfully."));
+  } catch (error) {
+    throw new ApiError(500, error.message || "Some Error Occurred.");
+  }
 });
 
 const updateComment = asyncHandler(async (req, res) => {
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,17 +1,70 @@
-import mongoose, { isValidObjectId } from "mongoose";
+import { isValidObjectId } from "mongoose";
 import { Like } from "../models/like.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-  const { videoId } = req.params;
-  //TODO: toggle like on video
+  try {
+    const { videoId } = req.params;
+    //TODO: toggle like on video
+    if (!isValidObjectId(videoId)) {
+      throw new ApiError(400, "Invalid videoId");
+    }
+
+    const likedAlready = await Like.findOne({
+      video: videoId,
+      likedBy: req.user?._id,
+    });
+
+    if (likedAlready) {
+      await Like.findByIdAndDelete(likedAlready?._id);
+
+      return res.status(200).json(new ApiResponse(200, { isLiked: false }));
+    }
+
+    await Like.create({
+      video: videoId,
+      likedBy: req.user?._id,
+    });
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, { isLiked: true }, "Successfully liked the video")
+      );
+  } catch (error) {
+    throw new ApiError(
+      500,
+      error.message || "Some error occurred while liking the video"
+    );
+  }
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
   const { commentId } = req.params;
-  //TODO: toggle like on comment
+
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid commentId");
+  }
+
+  const likedAlready = await Like.findOne({
+    comment: commentId,
+    likedBy: req.user?._id,
+  });
+
+  if (likedAlready) {
+    await Like.findByIdAndDelete(likedAlready?._id);
+
+    return res.status(200).json(new ApiResponse(200, { isLiked: false }));
+  }
+
+  await Like.create({
+    comment: commentId,
+    likedBy: req.user?._id,
+  });
+
+  return res.status(200).json(new ApiResponse(200, { isLiked: true }));
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
